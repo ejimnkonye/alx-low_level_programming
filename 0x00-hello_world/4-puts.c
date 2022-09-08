9fb81297123b17f08a956873f2d7af22166cebc5
ejimnkonye <ejimnkonyedon@gmail.com>
2022-09-08 12:23:26 -0700

    me

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
-  #include <stdio.h>
+#include <stdio.h>
 
-  /**
-   * main - entry point for the program
-   *
-   * Return: Displays a message to the terminal.
-   */
+ /**
+  * main -Entry point
+  *
+
+  *Return: Always 0 (Success)
+  */
   int main(void)
   {
-          puts("\"Programming is like building a multilingual puzzle");
-          return (0);
+
+
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
   }
