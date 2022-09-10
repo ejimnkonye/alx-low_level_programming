8f116187129251eb3b0fffa23561bf93a971e013
ejimnkonye <ejimnkonyedon@gmail.com>
2022-09-09 21:16:46 -0700

    me

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: Program that prints alphanets expecpt e and q
- * Return: Always 0
+ *
+ * Description: Program that prints alphabets except e and q
+ *
+ * Return: Always (0)
  */
 int main(void)
 {
-int i = 97;
-while (i < 123)
-{
-if (i != 101 && i != 113)
-{
-putchar(i);
+	int i = 97;
+
+	while (i < 123)
+	{
+	   if (i != 101 && i != 113)
+	   {
+	      putchar(i);
+
+	   }
+	   i++
+        }
+	putchar(10);
+	return(0);
 }
-i++;
-}
-putchar(10);
-return(0)
-}
+
