7a5b2eae8e466addeacbdda9619f904a32c94e25
ejimnkonye <ejimnkonyedon@gmail.com>
2022-09-10 08:54:56 -0700

    betty is wicked

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,7 +5,7 @@
  *Description: Write a program that prints all possible
  *different combinations of two digits
  *Numbers must be separated by ,, followed by a space
- *The two digits must be different 
+ *The two digits must be different
  *01 and 10 are considered the same combination of the two digits 0 and 1
  *Print only the smallest combination of two digits
  *Numbers should be printed in ascending order, with two digits
