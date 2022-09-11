f21f55f8b532aaaf074d8a82cd7a898846aa2b5b
ejimnkonye <ejimnkonyedon@gmail.com>
2022-09-11 15:46:29 -0700

    me

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Print numbers between 012 to 789
+ * main - Prints numbers between 012 to 789
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i, e, g;
+
 	i = 48;
 	e = 48;
 	g = 48;
+
 	while (e < 58)
 	{
 		i = 48;
