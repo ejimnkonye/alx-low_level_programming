ad5cf74ecc3917f28479dfc29605ea8a8502c2df
ejimnkonye <ejimnkonyedon@gmail.com>
2022-09-13 15:55:28 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet lowercase
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' <= 'z' i++);
+	{
+		_putchar(i);
+	}
+
+	putchar('\n');
+
+}
+
