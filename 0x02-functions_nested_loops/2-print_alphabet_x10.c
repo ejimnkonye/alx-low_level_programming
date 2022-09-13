e32fa2a68f881e73ac6d33570120d512c3aba56a
ejimnkonye <ejimnkonyedon@gmail.com>
2022-09-13 16:23:24 -0700

    my commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int count;
+
+
+	count = 0;
+	while (count < 10)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+
+		count++;
+		_putchar('\n');
+	}
+}
+
