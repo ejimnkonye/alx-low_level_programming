407e1f064db9826359ad18991cdb4eae3e5f9c83
ejimnkonye <ejimnkonyedon@gmail.com>
2022-09-20 04:40:30 -0700

    h

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,9 +3,7 @@
 #include <time.h>
 
 /**
- * main - program that generates random valid
- * password for the program 101-crackme
- *
+ * main - generates keygen
  * Return: Always 0 (Success)
  */
 
